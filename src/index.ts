6d00a782a3ae0a83d2deda4d9ea8eae6e8e2561b
brilhante14 <joao.brilhante@alu.ufc.br>
2025-01-17 17:14:58 -0300

    feat: project base file

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from 'dotenv';
+import bodyParser from "body-parser";
+import "express-async-errors";
+
+import { errorHandler } from "./middleware/errors";
+import { routes } from "./routes";
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json());
+
+app.use('/', routes);
+
+app.use(errorHandler);
+
+const port = 8080;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+});
+
